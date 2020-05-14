737845313c0caa34bf507f84c3221b29a6b6d085
sebnyawalo <snyawalo@gmail.com>
2020-05-14 23:25:33 +0300

    update data class

--- Data.py
+++ Data.py
@@ -1,11 +1,34 @@
 import csv
 
 class Read_data:
+    """
+    A class used to read data into the model
+    requires csv library to run.
+    ...
 
+    Attributes
+    ----------
+    file_name : str
+        a formatted string that represent the file name.
+
+    Methods
+    -------
+    read()
+        use the csv library to read data from a file;
+    """
     def __init__(self,file_name):
+        """
+        Parameters
+        ----------
+        name : str
+            the name of the file
+        """
         self.file_name = file_name
 
     def read(self):
+        """
+        reads and returns a list of the content of the file name passed.
+        """
         criteria =[]
         with open(self.file_name, mode='r') as csv_file:
             file_reader = csv.reader(csv_file, delimiter=",")
@@ -18,5 +41,6 @@ class Read_data:
                 line_count += 1
             # this is just to show how many lines are in the file
             print('Processed {} lines from txt file.'.format(line_count))
+            
         return criteria      
     
